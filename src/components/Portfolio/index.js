d532d2a206920df7fbf3ff21dca1f1a13b65977b
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-02-24 11:36:20 -0600

    Added 2 more projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,9 +7,21 @@ function Portfolio() {
   const [projects] = useState([
     {
       name: 'virtual-event-pro',
-      description: 'Online event planning app.',
+      description: 'Event planning app',
       link: "https://virtual-event-pro.herokuapp.com/",
       repo: "https://github.com/andersonmourot/virtual-event-pro"
+    },
+    {
+      name: 'work-day-scheduler',
+      description: 'Daily planner',
+      link: "https://andersonmourot.github.io/planner-project/",
+      repo: "https://github.com/andersonmourot/planner-project"
+    },
+    {
+      name: 'weather-dashboard',
+      description: 'Weather searching engine',
+      link: "https://andersonmourot.github.io/Weather-Dashboard/",
+      repo: "https://github.com/andersonmourot/Weather-Dashboard"
     }
   ]);
 
